2f78e519c9da14c8cf2b2dfcdda194a4f03de0e7
Alireza Tavakoli <alirezata3akoli@gmail.com>
2023-02-16 19:19:02 +0330

    Add a todo for naming

--- rewards/multinomial.py
+++ rewards/multinomial.py
@@ -5,7 +5,7 @@ import numpy as np
 from rewards.base import RewardBase
 
 
-# TODO: Better name
+# TODO: Choose a better name
 class MultinomialReward(RewardBase):
     def __init__(self, values: List[float], probabilities: List[float]) -> None:
         super(MultinomialReward, self).__init__()
