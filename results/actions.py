08caa114a6aa48f6c2230b008b9b9aa5482d148f
Alireza Tavakoli <alirezata3akoli@gmail.com>
2023-02-19 06:00:31 +0330

    Correct the spelling mistake

--- results/actions.py
+++ results/actions.py
@@ -2,16 +2,23 @@ import numpy as np
 
 
 class ActionsResult:
-    def __init__(self, repetition: int, trails: int) -> None:
+    def __init__(self, repetition: int, trials: int) -> None:
         self.repetition = repetition
-        self.trails = trails
-        self.actions = np.zeros((repetition, trails))
+        self.trials = trials
+        self.actions = np.zeros((repetition, trials))
+
+    def __init(self, repetition: int, trials: int, actions: np.array):
+        self.__init__(repetition=repetition, trials=trials)
+        self.actions = actions
+
+    def set_actions(self, actions: np.array):
+        self.actions = actions
 
     def get_repetition(self) -> int:
         return self.repetition
 
-    def get_trails(self) -> int:
-        return self.trails
+    def get_trials(self) -> int:
+        return self.trials
 
     def probability_of_choosing_action(self, desired_action: int) -> np.array:
         return np.mean(self.actions == desired_action, axis=0)
