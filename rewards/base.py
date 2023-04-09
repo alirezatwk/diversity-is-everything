3cbd283bad9a3a2b298c3ccf4868d31a0f950f05
Alireza Tavakoli <alirezata3akoli@gmail.com>
2023-04-09 14:23:35 +0330

    Add expected utility to rewards

--- rewards/base.py
+++ rewards/base.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
+from configs import SAMPLES_EXPECTED_UTILITY
+from utility_functions import UtilityFunctionBase
+
 
 class RewardBase(ABC):
     def __init__(self) -> None:
@@ -7,4 +12,16 @@ class RewardBase(ABC):
 
     @abstractmethod
     def get_reward(self) -> float:
-        pass
+        raise NotImplementedError()
+
+    def get_expected_utility(
+            self,
+            utility_function: UtilityFunctionBase,
+            samples: int = SAMPLES_EXPECTED_UTILITY
+    ) -> float:
+        utility_rewards = []
+        for sample in range(samples):
+            reward = self.get_reward()
+            utility_reward = utility_function.apply(reward)
+            utility_rewards.append(utility_reward)
+        return np.mean(utility_rewards)
