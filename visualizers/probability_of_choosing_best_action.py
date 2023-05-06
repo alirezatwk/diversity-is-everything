38c48c9bd815cf7fde61f4b43aa19d518ff7b286
Alireza Tavakoli <alirezata3akoli@gmail.com>
2023-05-06 14:45:03 +0330

    Implement error bar

--- visualizers/probability_of_choosing_best_action.py
+++ visualizers/probability_of_choosing_best_action.py
@@ -1,10 +1,11 @@
 import argparse
 import os
 import pickle
-from typing import List
+from typing import List, Tuple
 
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 
 
 class ProbabilityOfChoosingBestActionVisualizer:
@@ -14,8 +15,9 @@ class ProbabilityOfChoosingBestActionVisualizer:
         self.data_path = data_path
         self.write_path = write_path
 
-    def _read_results(self) -> dict:  # TODO: Use the correct type hint.
+    def _read_results(self) -> Tuple[dict, dict]:  # TODO: Use the correct type hint.
         agents_probabilities = {}
+        agents_errors = {}
         for file_name in sorted(os.listdir(self.data_path)):
             if 'pkl' not in file_name:
                 continue
@@ -23,9 +25,10 @@ class ProbabilityOfChoosingBestActionVisualizer:
                 agent_actions = pickle.load(pickle_file)
                 name = ''.join(file_name.split('.')[:-1])
                 agents_probabilities[name] = agent_actions.probability_of_choosing_action(self.best_action)
-        return agents_probabilities
+                agents_errors[name] = agent_actions.error_bound_of_choosing_action(self.best_action)
+        return agents_probabilities, agents_errors
 
-    def _get_pandas(self, agents_probabilities: dict) -> List[pd.DataFrame]:
+    def _get_pandas(self, agents_probabilities: dict, agents_errors: dict) -> List[pd.DataFrame]:
         agents_probabilities_pdf = []
         for agent in agents_probabilities:
             agent_max_trial = min(self.max_trial, len(agents_probabilities[agent]))
@@ -35,14 +38,53 @@ class ProbabilityOfChoosingBestActionVisualizer:
             )
             probabilities_pdf['trial'] = list(range(agent_max_trial))
             probabilities_pdf['agent'] = agent
+            probabilities_pdf['error'] = agents_errors[agent]
             agents_probabilities_pdf.append(probabilities_pdf)
         return agents_probabilities_pdf
 
-    def visualize(self):
-        agents_probabilities = self._read_results()
-        agents_probabilities_pdf = self._get_pandas(agents_probabilities=agents_probabilities)
-        agents_pdf = pd.concat(agents_probabilities_pdf)
+    def _get_figure(self, agents_pdf: pd.DataFrame): # TODO: use type hint
+        figure_with_error_bars = px.line(agents_pdf, x='trial', y='probability of choosing the best action', error_y='error',
+                                         color='agent')
         fig = px.line(agents_pdf, x='trial', y='probability of choosing the best action', color='agent')
+        for data in figure_with_error_bars.data:
+            x = list(data['x'])
+            y_upper = list(data['y'] + data['error_y']['array'])
+            y_lower = list(
+                data['y'] - data['error_y']['array'] if data['error_y']['arrayminus'] is None else data['y'] -
+                                                                                                   data['error_y'][
+                                                                                                       'arrayminus'])
+            color = f"rgba({tuple(int(data['line']['color'].lstrip('#')[i:i + 2], 16) for i in (0, 2, 4))},.3)".replace(
+                '((', '(').replace('),', ',').replace(' ', '')
+            fig.add_trace(
+                go.Scatter(
+                    x=x + x[::-1],
+                    y=y_upper + y_lower[::-1],
+                    fill='toself',
+                    fillcolor=color,
+                    line=dict(
+                        color='rgba(255,255,255,0)'
+                    ),
+                    hoverinfo="skip",
+                    showlegend=False,
+                    legendgroup=data['legendgroup'],
+                    xaxis=data['xaxis'],
+                    yaxis=data['yaxis'],
+                )
+            )
+        reordered_data = []
+        for i in range(int(len(fig.data) / 2)):
+            reordered_data.append(fig.data[i + int(len(fig.data) / 2)])
+            reordered_data.append(fig.data[i])
+        fig.data = tuple(reordered_data)
+        return fig
+
+    def visualize(self):
+        agents_probabilities, agents_errors = self._read_results()
+        agents_probabilities_pdf = self._get_pandas(agents_probabilities=agents_probabilities,
+                                                    agents_errors=agents_errors)
+        agents_pdf = pd.concat(agents_probabilities_pdf)
+
+        fig = self._get_figure(agents_pdf=agents_pdf)
         fig.write_html(self.write_path)
 
 
