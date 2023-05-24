4c93fefae80623d390e018bd5f77e3515cc47a98
Erfan Mirzaei <erfunmirzaei@gmail.com>
2023-05-24 23:08:43 +0430

    change analysis

--- Analysis.sh
+++ Analysis.sh
@@ -17,11 +17,11 @@ python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_
 python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_Diversity/configs/FE_2.config
 python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_Diversity/configs/FE_3.config
 
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best/configs/FE_1.config
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best/configs/FE_2.config
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best/configs/FE_3.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best_One_2Best/configs/FE_1.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best_One_2Best/configs/FE_2.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Best_One_2Best/configs/FE_3.config
 
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_Two_Random/configs/FE_1.config
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_Two_Random/configs/FE_2.config
-python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_Two_Random/configs/FE_3.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Random_One_Worst/configs/FE_1.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Random_One_Worst/configs/FE_2.config
+python Analysis.py -c /home/erfan/Projects/diversity/Analysis/M1_1_1/One_Social_One_Learner_One_Random_One_Worst/configs/FE_3.config
 
